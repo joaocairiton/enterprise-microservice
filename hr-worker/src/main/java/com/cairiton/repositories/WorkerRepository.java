0cf963d3e2e76b5834a966c98c9e38a505f21869
joaocairiton <joaocairiton@gmail.com>
2023-04-24 17:10:54 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/cairiton/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.cairiton.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cairiton.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
