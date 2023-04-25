d0286576dee5b243a1482f4b6cde8010c31d283c
joaocairiton <joaocairiton@gmail.com>
2023-04-25 09:29:40 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/cairiton/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.cairiton.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+	
+	
+	
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new  RestTemplate();
+	}
+	
+	
+	
+	
+
+}
