3fc8461061628682ed5de31f0785931dda2a9b10
joaocairiton <joaocairiton@gmail.com>
2023-04-26 16:29:02 -0300

    hr-config-server created

---
+++ hr-config-server/src/test/java/com/cairiton/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cairiton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
