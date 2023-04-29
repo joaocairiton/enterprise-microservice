ef030171597c48ad82dd6fd845ccaab5e32932d1
joaocairiton <joaocairiton@gmail.com>
2023-04-29 15:24:36 -0300

    Login e geração do Token Jwt

--- hr-oauth/src/main/java/com/cairiton/services/UserService.java
+++ hr-oauth/src/main/java/com/cairiton/services/UserService.java
@@ -3,22 +3,25 @@ package com.cairiton.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.cairiton.entities.User;
 import com.cairiton.feignClients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	
 	@Autowired
-	private UserFeignClient feignClient;
+	private UserFeignClient userfeignClient;
 	
 	public User findByEmail(String email) {
-		User user = feignClient.findByEmail(email).getBody();
+		User user = userfeignClient.findByEmail(email).getBody();
 		if (user == null) {
 			
 			logger.error("email not found: " + email);
@@ -30,4 +33,17 @@ public class UserService {
 		
 	}
 
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userfeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			
+			logger.error("email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info(" Email found: " + username);
+		return user;
+	}
+
+	
 }
