b47788872b73dba34aa1d67704f3bd572262288e
joaocairiton <joaocairiton@gmail.com>
2023-05-06 11:46:24 -0300

    Foi Adcionado methodo Post

--- hr-worker/src/main/java/com/cairiton/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/cairiton/resources/WorkerResource.java
@@ -2,65 +2,72 @@ package com.cairiton.resources;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cairiton.entities.Worker;
 import com.cairiton.repositories.WorkerRepository;
 
-
-
-
-@RefreshScope
+//@RefreshScope
 @RestController
-@RequestMapping(value ="/workers")
+@RequestMapping(value = "/workers")
 public class WorkerResource {
-	
-	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
-	@Value("${test.config}")
-	private String testConfig;
-	
+
+	//private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+	/*
+	 * @Value("${test.config}") private String testConfig;
+	 */
+
 	@Autowired
 	private Environment env;
-	
-	
+
 	@Autowired
 	private WorkerRepository workerRepository;
-	
-	@GetMapping(value= "/configs")
-	public ResponseEntity<Void> getConfigs(){
-		logger.info("CONFIG = " + testConfig);
-		return ResponseEntity.noContent().build();
-	}
-	
+
+	/*
+	 * @GetMapping(value= "/configs") public ResponseEntity<Void> getConfigs(){
+	 * logger.info("CONFIG = " + testConfig); return
+	 * ResponseEntity.noContent().build(); }
+	 */
+
 	@GetMapping
-	public ResponseEntity<List<Worker>> findAll(){
+	public ResponseEntity<List<Worker>> findAll() {
 		List<Worker> list = workerRepository.findAll();
 		return ResponseEntity.ok(list);
 	}
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<Worker> findById(@PathVariable Long id){
+	/*
+	 * @GetMapping(value = "/{id}") public ResponseEntity<Worker>
+	 * findById(@PathVariable Long id){
+	 * 
+	 * 
+	 * try { //Thread.sleep(3000L); } catch (InterruptedException e) { // TODO
+	 * Auto-generated catch block e.printStackTrace(); }
+	 * 
+	 * 
+	 * 
+	 * logger.info("PORT = " + env.getProperty("local.server.port"));
+	 * 
+	 * Worker obj = workerRepository.findById(id).get(); return
+	 * ResponseEntity.ok(obj); }
+	 */
+	
+	
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Worker adicionar(@RequestBody Worker worker) {
+		return workerRepository.save(worker);
 		
-		/*
-		 * try { //Thread.sleep(3000L); } catch (InterruptedException e) { // TODO
-		 * Auto-generated catch block e.printStackTrace(); }
-		 */
-		
-		
-		logger.info("PORT = " + env.getProperty("local.server.port"));
-		
-		Worker obj = workerRepository.findById(id).get();
-		return ResponseEntity.ok(obj);
 	}
 
 }
